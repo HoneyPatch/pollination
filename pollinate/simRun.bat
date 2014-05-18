f2ccc3c3c896aaf456910c9306c5e40c8975568d
Chris Mackey <chris@mackeyarchitecture.com>
2014-05-18 09:22:39 -0400

    Mostapha's OSM File

---
+++ pollinate/simRun.bat
@@ -0,0 +1,3 @@
+C:
+cd\Users\cwmackey\Documents\pollinator\pollinate\
+C:\EnergyPlusV8-1-0\Epl-run C:\Users\cwmackey\Documents\pollinator\pollinate\simRun C:\Users\cwmackey\Documents\pollinator\pollinate\simRun idf C:\ladybug\USA_NY_New\USA_NY_New.York-J.F.Kennedy.Intl.AP.744860_TMY3.epw EP N nolimit N N 0 Y
